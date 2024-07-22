a821021de25b0a2a3f29cf036f6b1edc7e5ed992
peace <peacebizzy32@gmail.com>
2024-07-22 15:51:30 +0100

    responsiveness

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from "../component/NavBar"
 import Blog from './Blog'
 import Footer from './Footer'
-// import SideNav from './SideNav'
+import "../css/Hero.css"
+import SideNav from './SideNav'
 import Background from "../images/mainbg.png" 
 
 const Hero = () => {
+    const [bedroomCount, setBedroomCount] = useState(0);
+    
+      const increment = () => setBedroomCount(bedroomCount + 1);
+  const decrement = () => {
+    if (bedroomCount > 0) {
+      setBedroomCount(bedroomCount - 1);
+    }
+  };
+
   return (
     <>
     <div style={{backgroundImage:   `url(${Background})`, backgroundSize: "cover"}} className='hero w-full h-full'>
         <NavBar/>
-        {/* <SideNav/> */}
+        <SideNav/>
         <div className="main-hero w-full h-screen xl:mt-44 xl:h-full flex flex-col gap-16 ">
            <div className="header pt-20 xl:w-4/5 xl:m-auto text-center flex flex-col gap-6 xl:pt-0">
-            <h1 className='hero-head font-[black] text-xl xl:text-7xl text-white xl:font-bold xl:p-0'>Browse Our Properties</h1>
-            <p className='hero-para text-2xl xl:font-normal xl:text-2xl text-white '>Find your perfect home among our curated properties. Start <br /> browsing now!</p>
+            <h1 className='hero-head mt-32 font-[black] text-4xl xl:text-7xl text-white xl:font-bold xl:p-0'>Browse Our Properties</h1>
+            <p className='hero-para text-3xl xl:font-normal xl:text-2xl text-white '>Find your perfect home among our curated properties. Start <br /> browsing now!</p>
            </div>
 
            <div className="hero-info w-4/5 h-full m-auto xl:w-4/5 xl:h-36 xl:grid xl:grid-cols-4 xl:p-4 xl:m-auto xl:place-content-center xl:backdrop-blur-sm xl:bg-white/20 mb-4">
@@ -31,9 +41,9 @@ const Hero = () => {
             <div className="three flex flex-col bg-white text-black h-20 justify-center items-center">
                 <h4 className='mb-1'>BEDROOM</h4>
                 <div className="yea flex gap-8">
-                    <p style={{height: "23px", width: '20px', border: "1px solid black", borderRadius: "50%",  display: 'flex', justifyContent: "center", alignItems: "center"}}>-</p>
-                    <p>0</p>
-                    <small style={{ height: "23px", width: '20px', border: "1px solid black", borderRadius: "50%", display: 'flex', justifyContent: "center", alignItems: "center"}}>+</small>
+                    <button style={{height: "23px", width: '20px', border: "1px solid black", borderRadius: "50%",  display: 'flex', justifyContent: "center", alignItems: "center"}} onClick={decrement}>-</button>
+                    <p>{bedroomCount}</p>
+                    <button style={{ height: "23px", width: '20px', border: "1px solid black", borderRadius: "50%", display: 'flex', justifyContent: "center", alignItems: "center"}} onClick={increment}>+</button>
                 </div>
             </div>
 
--- src/component/SideNav.jsx
+++ src/component/SideNav.jsx
@@ -1,69 +1,69 @@
-// import React, { useState, useEffect, useRef } from 'react'
-// import { Link } from 'react-router-dom'
-// import logo from '../images/Group 9282 (1).png'
-// import hamburger from '../images/icon-hamburger.svg'
-// import close from '../images/icon-close.svg'
-// import "../css/SideNav.css"
+import React, { useState, useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../images/Group 9282 (1).png'
+import hamburger from '../images/icon-hamburger.svg'
+import close from '../images/icon-close.svg'
+import "../css/SideNav.css"
 
-// const SideNav = () => {
-//   const [menu, setMenu] = useState(false)
+const SideNav = () => {
+  const [menu, setMenu] = useState(false)
 
-//   let menuRef = useRef();
+  let menuRef = useRef();
 
-//   useEffect(()=>{
-//     document.addEventListener('mousedown', (event)=>{
-//       if(!menuRef.current.contains(event.target)){
-//         setMenu(false)
-//       }
-//     })
-//   })
+  useEffect(()=>{
+    document.addEventListener('mousedown', (event)=>{
+      if(!menuRef.current.contains(event.target)){
+        setMenu(false)
+      }
+    })
+  })
 
-//  console.log(menu);
-//   return (
-//     <div className='sidebar' ref={menuRef}>
-//       <div className="left-logo">
-//         <img src={logo} alt="logo" />
-//       </div>
-//       <div className="menu">
-//         <div className="img-side" onClick={()=>{
-//           setMenu(!menu)
-//         }}>
-//           {menu ? (<img src={close} alt="" className='open'/>) : (<img src={hamburger} alt="" className='close' />)}
+ console.log(menu);
+  return (
+    <div className='sidebar' ref={menuRef}>
+      <div className="left-logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="menu">
+        <div className="img-side" onClick={()=>{
+          setMenu(!menu)
+        }}>
+          {menu ? (<img src={close} alt="" className='open'/>) : (<img src={hamburger} alt="" className='close' />)}
         
         
-//         </div>
-//        {menu ?  (<div className="side">
-//         <ul>
-//           <Link to='/' style={{textDecoration:"none"}}>
-//             <div className="side-btn">
-//               <li>Home</li>
-//             </div>
-//           </Link>
-//           <Link to='/' style={{textDecoration:"none"}}>
-//             <div className="side-btn">
-//               <li>About</li>
-//             </div>
-//           </Link>
-//           <Link to='/' style={{textDecoration:"none"}}>
-//             <div className="side-btn">
-//               <li>Properties</li>
-//             </div>
-//           </Link>
-//           <Link to='/' style={{textDecoration:"none"}}>
-//             <div className="side-btn">
-//               <li>Contact Us</li>
-//             </div>
-//           </Link>
-//         </ul>
-//       </div>) :
+        </div>
+       {menu ?  (<div className="side">
+        <ul>
+          <Link to='/' style={{textDecoration:"none"}}>
+            <div className="side-btn">
+              <li>Home</li>
+            </div>
+          </Link>
+          <Link to='/' style={{textDecoration:"none"}}>
+            <div className="side-btn">
+              <li>About</li>
+            </div>
+          </Link>
+          <Link to='/' style={{textDecoration:"none"}}>
+            <div className="side-btn">
+              <li>Properties</li>
+            </div>
+          </Link>
+          <Link to='/' style={{textDecoration:"none"}}>
+            <div className="side-btn">
+              <li>Contact Us</li>
+            </div>
+          </Link>
+        </ul>
+      </div>) :
       
-//     (<div className=""></div> )}
+    (<div className=""></div> )}
        
-//       </div>
+      </div>
 
       
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default SideNav
+export default SideNav
